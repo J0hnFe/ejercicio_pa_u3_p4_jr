8b788292be53fb7959834aed6943d9e70eeef9cf
John <johnfe@outlook.es>
2023-08-07 18:28:20 -0500

    Commit inicial

---
+++ src/test/java/com/example/demo/EjercicioPaU3P4JrApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EjercicioPaU3P4JrApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
